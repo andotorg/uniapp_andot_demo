a1836ffc3ab4d75d43c07e146b32df8fdd1a03da
andot <yanfront@qq.com>
2019-07-10 16:23:31 +0800

    媒体图片显示组件,支持属性设置加载失败显示默认图片image

---
+++ components/an-image/an-image.vue
@@ -0,0 +1,46 @@
+<template>
+	<view>
+		<image :src="url" :mode="mode" @error="imgError" :style="'width:'+(width<1?width*100+'%;':width+'px;')+'; height: '+(height<1?height*100+'%;':height+'px;')"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			src: {
+				type: String,
+				default: ''
+			},
+			alt: {
+				type: String,
+				default: ''
+			},
+			mode: {
+				type: String,
+				default: ''
+			},
+			width: {
+				type: Number,
+				default: 70
+			},
+			height: {
+				type: Number,
+				default: 90
+			}
+		},
+		data() {
+			return {
+				url: this.src
+			};
+		},
+		methods: {
+			imgError(){
+				this.url = this.alt;
+			},
+		}
+	}
+</script>
+
+<style>
+
+</style>
